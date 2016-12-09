12c8aa61bd021728dd83a227058b2bd8b42408be
yihanseattle <yihanseattle@gmail.com>
2016-12-08 16:39:59 -0800

    L48.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
 
@@ -30,11 +31,16 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image
           style={imageStyle}
           source={{ uri: image }}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   );
