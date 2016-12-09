4fd1836fa866b7934dfa23acb40fe12f1afbdb50
yihanseattle <yihanseattle@gmail.com>
2016-12-08 16:15:02 -0800

    L45.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -7,22 +7,34 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
 
-  const { title, artist, thumbnail_image } = album;
-  const {thumbnailStyle, headerContentStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View >
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle }
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
       </CardSection>
     </Card>
   );
@@ -33,11 +45,24 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
-
 };
 
 export default AlbumDetail;
